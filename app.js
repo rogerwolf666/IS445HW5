85a20890fcbe7cd785d7c1ba9bfe3ab415be8841
VasiliyVolkov <vasiliy.volkov88@gmail.com>
2020-05-02 09:12:31 -0700

    Formatting

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ formElement.addEventListener("submit", e => {
   var small;
   var large;
   let primeNums = [];
-  var k;
+  var divideCheck;
 
   if(numOne > numTwo){
     small = numTwo;
@@ -14,23 +14,23 @@ formElement.addEventListener("submit", e => {
   } 
   else{
     small = numOne
-  large = numTwo
-}
+    large = numTwo
+  }
 
-  if((numOne < 2 || numOne > 100||numTwo < 2 || numTwo > 100 || isNaN(numOne) || isNaN(numTwo))){document.getElementById("output").innerHTML=`<p>Invalid input, please try again</p>`;}
+  if((numOne < 2 || numOne > 100||numTwo < 2 || numTwo > 100 || isNaN(numOne) || isNaN(numTwo))){document.getElementById("output").innerHTML=`<p class="text-danger">Invalid input, please try again</p>`;}
 
   else {
     for(var i = small; i<=large;i+=1){
-      k=0
+      divideCheck=0
       for(var j = 2; j<i;j+=1){
-        if(i%j===0){k+=1}
+        if(i%j===0){divideCheck+=1}
       }
-      if(k===0){primeNums.push(i)}
+      if(divideCheck===0){primeNums.push(i)}
     }
-    document.getElementById("output").innerHTML=`<p>There are ${primeNums.length} prime numbers.</p>`;
+    document.getElementById("output").innerHTML=`<p class="text-info">There are ${primeNums.length} prime numbers.</p>`;
 
     if(primeNums.length > 0){
-      document.getElementById("output").innerHTML+=`<p>${primeNums.toString()}</p>`;
+      document.getElementById("output").innerHTML+=`<p class="text-danger font-weight-bold">${primeNums.toString()}</p>`;
     }
   
   
